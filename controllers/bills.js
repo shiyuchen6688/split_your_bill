43636650ea1259e9a808a94eacae487698f70108
shiyuc <shiyuc@students.cs.ubc.ca>
2021-10-27 11:05:09 -0700

    stub for bills controllers done

---
+++ controllers/bills.js
@@ -0,0 +1,25 @@
+const getAllBills = (req, res) => {
+    res.send("all bills recorded");
+}
+
+const addNewBill = (req, res) => {
+    res.send("all bills recorded");
+}
+const getOneBill = (req, res) => {
+    res.send("all bills recorded");
+}
+const updateBill = (req, res) => {
+    res.send("all bills recorded");
+}
+const deleteBill = (req, res) => {
+    res.send("all bills recorded");
+}
+
+
+module.exports = {
+    getAllBills,
+    addNewBill,
+    getOneBill,
+    updateBill,
+    deleteBill
+}
--- routers/bills.js
+++ routers/bills.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.route("/").get((req, res) => {
-    res.send("all items");
-})
+const { getAllBills, addNewBill, getOneBill, updateBill, deleteBill } = require("../controllers/bills.js")
+
+router.route("/").get(getAllBills).post(addNewBill)
+
+router.route("/:id").get(getOneBill).patch(updateBill).delete(deleteBill)
+
 
 module.exports = router
